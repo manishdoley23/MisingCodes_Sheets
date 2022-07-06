9f61e65a36704adbc3e85670c70d81a191332902
Manish Doley <98950901+manishdoley23@users.noreply.github.com>
2022-07-06 13:36:04 +0530

    Create Search a 2D Matrix.cpp

---
+++ Striver’s Interview Problems Sheet/Arrays/Search a 2D Matrix.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+public:
+    bool binSearch(vector<int> vec, int l, int r, int t){
+        while(l <= r){
+            int mid = (l+r)/2;
+            
+            if(vec[mid] == t){
+                return 1;
+            }else if(vec[mid] > t){
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return 0;
+    }
+    
+    bool searchMatrix(vector<vector<int>>& m, int t) {
+        int rowLastInd = m.size() - 1;
+        int colLastInd = m[m.size()-1].size() - 1;
+        for(int i=0; i<rowLastInd; ++i){
+            if(t == m[i][0] || t == m[i+1][0]){
+                return 1;
+            }else if(t > m[i][0] && t < m[i+1][0]){
+                int j = 1;
+                return binSearch(m[i], j, m[i].size()-1, t);
+            }
+        }
+        
+        // cout << "rowLastInd:" << rowLastInd << endl;
+        // cout << "colLastInd:" << colLastInd << endl;
+        
+        return binSearch(m[rowLastInd], 0, colLastInd, t);
+    }
+};
